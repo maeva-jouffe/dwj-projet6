c15abfc94532996550184634fbf1eb66b0f8d96a
maeva-jouffe <maeva.jouffe@hotmail.fr>
2021-07-12 11:37:36 +0200

    Ajout de modules de sécurité

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+
+//Modules pour la sécurité
+const dotenv = require('dotenv').config();
+const urlMasquee = process.env.MONGODB_URL;
+const mongoSanitize = require('express-mongo-sanitize');
+
+//Paramètres des routes
 const sauceRoutes = require('./routes/sauces')
 const userRoutes = require('./routes/user');
 
-mongoose.connect('mongodb+srv://maeva:marseille13007@cluster0.gxnuk.mongodb.net/sopekocko?retryWrites=true&w=majority',
+//Initialisation d'express
+const app = express();
+
+//connexion à la base de données
+mongoose.connect(urlMasquee,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-const app = express();
-
+//Configuration des headers CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -22,8 +32,14 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
+//Ajout du gestionnaire de routage pour rendre le dossier images statique
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
+//Ajout du module mongoSanitize
+app.use(mongoSanitize());
+
+
+//Routes
 app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 
--- backend/middleware/passwordValidator.js
+++ backend/middleware/passwordValidator.js
@@ -1,5 +1,6 @@
 const passwordSchema = require("../models/passwordValidator");
 
+
 module.exports = (req, res, next) => {
     if(!passwordSchema.validate(req.body.password)){
         res.status(400).json({error : "le mot de passe n'est pas assez sécurisé : "});
