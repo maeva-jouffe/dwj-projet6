e4c5dfa6e78b682a306d6b884cbbe59227d7143b
maeva-jouffe <maeva.jouffe@hotmail.fr>
2021-07-20 09:14:30 +0200

    augmentation du nombre de caractères requis pour le mot de passe

--- backend/models/passwordValidator.js
+++ backend/models/passwordValidator.js
@@ -2,8 +2,8 @@ const passwordValidator  = require ('password-validator');
 const passwordSchema = new passwordValidator();
 
 passwordSchema
-//Doit contenir minimum 5 caractères
-.is().min(5)
+//Doit contenir minimum 8 caractères
+.is().min(8)
 //Doit contenir maximum 20 caractères
 .is().max(20)
 //Doit contenir au moins une lettre majuscule
